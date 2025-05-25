d40709387fdaf7a9a8128201ae8b157a360d8199
maruf_hossain <marufhossain123098@gmail.com>
2025-05-25 23:26:08 +0600

    class based view added

--- author/views.py
+++ author/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect
 from . import forms
+from django.urls import reverse_lazy
 from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm
 from django.contrib.auth import authenticate,login,update_session_auth_hash,logout 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from posts.models import Post
+from django.contrib.auth.views import LoginView,LogoutView
 # Create your views here.
 
 # def add_author(request):
@@ -48,6 +50,32 @@ def user_login(request):
     else:
         form = AuthenticationForm()
         return render(request, 'register.html',{'form': form, 'type' : 'Login'})
+
+
+
+class UserLoginView(LoginView):
+    template_name = 'register.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('profile')
+
+    def form_valid(self,form):
+        messages.success(self.request,'Logged in successfully')
+        return super().form_valid(form)
+    
+    def form_invalid(self,form):
+        messages.warning(self.request,'Logged in information incorrect')
+        return super().form_invalid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['type'] = 'Login'
+        return context
+    
+
+
+
+
 
 
 @login_required
--- posts/forms.py
+++ posts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post,Comment
 
 
 class PostForm(forms.ModelForm):
@@ -8,6 +8,14 @@ class PostForm(forms.ModelForm):
         # fields = '__all__'
         exclude = ['author']
 
-      
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name','email', 'body']
+       
+
+           
+
 
 
